30d4509ca0ac15299415695d69b90b829a2c9884
Ikhlaq Sidhu <sidhu@berkeley.edu>
2025-06-19 17:39:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).
-
-Line 2: added for fun"
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)"
 
 )
